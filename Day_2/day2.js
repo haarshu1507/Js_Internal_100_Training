061617fad8e2598f59f96415b91518d968bc9803
Harsh Sharma <haarshlapi1507@gmail.com>
2025-12-09 05:31:57 +0000

    day2

---
+++ Day_2/day2.js
@@ -0,0 +1,9 @@
+let a= 23
+const b= "abc"
+var c= 78
+d=88
+
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
---
+++ Day_2/var.js
@@ -0,0 +1,20 @@
+/*let a="abc"
+let b=""
+let c=null
+let d;
+let e =undefined
+console.log("value of a:", a)
+console.log("value of a:",b)
+console.log("value of a:",c)
+console.log("value of a:",d)
+console.log("value of a:",e)
+console.log(typeof(a))
+console.log(typeof(d))
+console.log(typeof(c))*/
+
+let score = "33aabbcc" 
+console.log(typeof(score))
+console.log(typeof score)
+let score2 = Number(score)
+console.log(typeof(score2))
+console.log("value of score:", score)
