bb4c459416de153625e7ba902bfa291a7783c320
Harsh Sharma <haarshlapi1507@gmail.com>
2025-12-20 09:53:11 +0000

    task1_onlyjs code

---
+++ Day4/Task1.js
@@ -0,0 +1,8 @@
+<p> id="text">Hello</p>
+<button onclick="changeText()">Click Me</button>
+
+<script>
+function changeText() {
+    document.getElementById("text").innerText = "Welcome to JavaScript!";
+}
+</script>
